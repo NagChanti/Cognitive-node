bee7dba7a05a3f9f0d9eb7eb1e1ef578563a09e8
NagChanti <chapalanagendrababu@gmail.com>
2016-07-26 18:19:08 +0530

    Add files via upload

---
+++ app1.js
@@ -0,0 +1,64 @@
+var builder = require('botbuilder');
+var restify = require('restify');
+
+//=========================================================
+// Bot Setup
+//=========================================================
+
+//Setup Restify Server
+//var ip_addr = 'localhost';
+var port    =  '3978';
+var server = restify.createServer();
+server.listen(port, function () {
+	//host: 'localhost';
+	console.log('%s listening to %s', server.name, server.url); 
+});
+
+  
+// Create chat bot
+var connector = new builder.ChatConnector({
+    appId: process.env.MICROSOFT_APP_ID,
+    appPassword: process.env.MICROSOFT_APP_PASSWORD
+});
+var bot = new builder.UniversalBot(connector);
+//server.use(bot.verifyBotFramework());
+server.post('/api/messages', connector.listen());
+
+//=========================================================
+// Bots Dialogs
+//=========================================================
+
+var intents = new builder.IntentDialog();
+bot.dialog('/', intents);
+
+intents.matches(/^change name/i, [
+    function (session) {
+        session.beginDialog('/profile');
+    },
+    function (session, results) {
+        session.send('Ok... Changed your name to %s', session.userData.name);
+    }
+]);
+
+intents.onDefault([
+    function (session, args, next) {
+        if (!session.userData.name) {
+            session.beginDialog('/profile');
+        } else {
+            next();
+        }
+    },
+    function (session, results) {
+        session.send('Hello %s!', session.userData.name);
+    }
+]);
+
+bot.dialog('/profile', [
+    function (session) {
+        builder.Prompts.text(session, 'Hi! What is your name?');
+    },
+    function (session, results) {
+        session.userData.name = results.response;
+        session.endDialog();
+    }
+]);
